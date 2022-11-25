20080489111e44d91d3dd89e5f03f22c84ce93c1
Zhasmina Aitmagambetova <aitmazh@Zhasminas-MacBook-Air.local>
2022-11-26 00:40:09 +0600

    some changes

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -8,95 +8,103 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Sign in to the system',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
-            ),
+      body: Column(
+        children: [
+          /* const IconButton(
+            onPressed: onPressed, 
+            icon: Icon(Icons.settings)), */
 
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Do not have an account?',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Sign in to the system',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
 
-                  // --------------------------- Register text button
-                  TextButton(
-                      style: TextButton.styleFrom(
-                        textStyle: const TextStyle(
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Do not have an account?',
+                        style: TextStyle(
                           fontSize: 16,
+                          fontWeight: FontWeight.normal,
                         ),
                       ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const RegisterPage()),
-                        );
-                      },
-                      child: const Text('Register now'))
-                ],
-              ),
-            ),
 
-            // --------------------------------- Username textfield
-            Container(
-              constraints: const BoxConstraints(maxWidth: 400),
-              margin: const EdgeInsets.symmetric(horizontal: 200, vertical: 10),
-              decoration: BoxDecoration(
-                color: Colors.grey[200]!,
-                border: Border.all(color: Colors.grey[300]!),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10),
-                  border: InputBorder.none,
-                  hintText: 'username',
+                      // --------------------------- Register text button
+                      TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const RegisterPage()),
+                            );
+                          },
+                          child: const Text('Register now'))
+                    ],
+                  ),
                 ),
-              ),
-            ),
 
-            // --------------------------------- Password textfield
-            Container(
-              constraints: const BoxConstraints(maxWidth: 400),
-              margin: const EdgeInsets.symmetric(horizontal: 200, vertical: 10),
-              decoration: BoxDecoration(
-                color: Colors.grey[200]!,
-                border: Border.all(color: Colors.grey[300]!),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10),
-                  border: InputBorder.none,
-                  hintText: 'password',
+                // --------------------------------- Username textfield
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 400),
+                  margin: const EdgeInsets.symmetric(horizontal: 200, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200]!,
+                    border: Border.all(color: Colors.grey[300]!),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10),
+                      border: InputBorder.none,
+                      hintText: 'username',
+                    ),
+                  ),
                 ),
-              ),
-            ),
 
-            MainButtonView(
-              text: 'Sign in',
-              function: _login,
-            ),
+                // --------------------------------- Password textfield
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 400),
+                  margin: const EdgeInsets.symmetric(horizontal: 200, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200]!,
+                    border: Border.all(color: Colors.grey[300]!),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10),
+                      border: InputBorder.none,
+                      hintText: 'password',
+                    ),
+                  ),
+                ),
 
-            // --------------------------------- Google sign in button
-          ],
-        ),
+                MainButtonView(
+                  text: 'Sign in',
+                  function: _login,
+                ),
+
+                // --------------------------------- Google sign in button
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
